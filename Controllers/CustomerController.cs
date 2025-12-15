b2ff0c1c22bd6e7bf9aa775713ce5c81f491646b
iftychowdhury255 <iftychowdhury10212089@gmail.com>
2025-12-16 02:50:11 +0600

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CustomerCoreApi.Data;
+using CustomerCoreApi.Models;
+
+namespace CustomerCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext _dbContext;
+        public CustomerController(CustomerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            var customers = _dbContext.Customers.ToList();
+            return Ok(customers);
+        }
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetCustomerID(Guid id)
+        {
+            var customer = _dbContext.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            var newCustomer = new Customer()
+            {
+                Name = customer.Name,
+                Email = customer.Email,
+                Address = customer.Address,
+                TotalOrders = customer.TotalOrders
+            };
+            _dbContext.Customers.Add(newCustomer);
+            _dbContext.SaveChanges();
+            return Ok(newCustomer);
+        }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateCustomer(Guid id, Customer customer)
+        {
+            var existingCustomer = _dbContext.Customers.Find(id);
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+            existingCustomer.Name = customer.Name;
+            existingCustomer.Email = customer.Email;
+            existingCustomer.Address = customer.Address;
+            existingCustomer.TotalOrders = customer.TotalOrders;
+            _dbContext.SaveChanges();
+            return Ok(existingCustomer);
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteCustomer(Guid id)
+        {
+            var existingCustomer = _dbContext.Customers.Find(id);
+            if (existingCustomer == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Customers.Remove(existingCustomer);
+            _dbContext.SaveChanges();
+            return Ok(existingCustomer);
+        }
+    }
+}
---
+++ Data/CustomerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomerCoreApi.Models;
+
+namespace CustomerCoreApi.Data
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       public DbSet<Customer> Customers { get; set; }
+    }
+}
